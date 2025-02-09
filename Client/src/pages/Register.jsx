7f3f4d972b6232c12a9981a4fb6ea1c5a6edac5a
RohanPengonda <rpengonda1@gmail.com>
2025-02-09 15:03:42 +0530

    Frontend

--- Client/src/pages/Register.jsx
+++ Client/src/pages/Register.jsx
@@ -1,20 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaEyeSlash } from "react-icons/fa6";
+import { FaRegEye } from "react-icons/fa";
 
 const Register = () => {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const validateValue = Object.values(data).every((el) => el);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
   return (
     <section className="w-full container mx-auto px-2">
       <div className="bg-white my-4 w-full max-w-lg mx-auto rounded p-4">
         <p>Welcome to BlinkeyIt....</p>
-        <form action="" className="grid gap-2 mt-6">
-          <div>
+        <form action="" className="grid gap-4 mt-6">
+          <div className="grid gap-1">
             <label htmlFor="name">Name: </label>
             <input
               type="text"
+              id="name"
+              name="name"
               autoFocus
               placeholder="Enter Name"
-              className="'bg-blue-50 p-2"
+              className="bg-blue-50 p-2 border rounded outline-none focus:border-primary-200"
+              value={data.name}
+              onChange={handleChange}
             />
           </div>
+          <div className="grid gap-1">
+            <label htmlFor="email">Email: </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              placeholder="Enter Email"
+              className="bg-blue-50 p-2 border rounded outline-none focus:border-primary-200"
+              value={data.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="grid gap-1">
+            <label htmlFor="password">Password: </label>
+            <div className="bg-blue-50 p-2 border rounded flex items-center focus-within:border-primary-200">
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                name="password"
+                autoFocus
+                placeholder="Enter Password"
+                className="w-full outline-none"
+                value={data.password}
+                onChange={handleChange}
+              />
+              <div
+                onClick={() => setShowPassword((prev) => !prev)}
+                className="cursor-pointer"
+              >
+                {showPassword ? <FaRegEye /> : <FaEyeSlash />}
+              </div>
+            </div>
+          </div>
+          <div className="grid gap-1">
+            <label htmlFor="confirmPassword">Confirm Password: </label>
+            <div className="bg-blue-50 p-2 border rounded flex items-center focus-within:border-primary-200">
+              <input
+                type={showConfirmPassword ? "text" : "password"}
+                id="confirmPassword"
+                name="confirmPassword"
+                autoFocus
+                placeholder="Enter Password"
+                className="w-full outline-none"
+                value={data.confirmPassword}
+                onChange={handleChange}
+              />
+              <div
+                onClick={() => setShowConfirmPassword((prev) => !prev)}
+                className="cursor-pointer"
+              >
+                {showConfirmPassword ? <FaRegEye /> : <FaEyeSlash />}
+              </div>
+            </div>
+          </div>
+
+          <button
+            className={`${
+              validateValue ? "bg-green-800" : "bg-gray-500 "
+            }   text-white py-2 rounded font-semibold my-3 tracking-wide`}
+          >
+            Register
+          </button>
         </form>
       </div>
     </section>
