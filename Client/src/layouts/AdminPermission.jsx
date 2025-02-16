abe331ffcc7ea7f7861fb08253aaece4cadaf087
RohanPengonda <rpengonda1@gmail.com>
2025-02-16 19:35:13 +0530

    Updated

--- Client/src/layouts/AdminPermission.jsx
+++ Client/src/layouts/AdminPermission.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import isAdmin from "../utils/isAdmin";
 
-const AdminPermission = (children) => {
+const AdminPermission = ({ children }) => {
   const user = useSelector((state) => state.user);
   return (
     <div>
