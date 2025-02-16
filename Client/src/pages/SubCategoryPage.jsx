9de954c6073c7c4d6259e1706a9f6b54abddebb0
RohanPengonda <rpengonda1@gmail.com>
2025-02-16 19:42:08 +0530

    Updated

--- Client/src/pages/SubCategoryPage.jsx
+++ Client/src/pages/SubCategoryPage.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import UploadSubCategoryModel from "../components/UploadSubCategoryModel";
 
 const SubCategoryPage = () => {
+  const [openAddSubCategory, setOpenAddSubCategory] = useState(false);
   return (
-    <div>
+    <section className="">
       <div className="py-2  bg-white shadow-md flex items-center justify-between">
         <h2 className="font-semibold">Sub-Category</h2>
-        <button className="text-sm border border-primary-200 hover:bg-primary-200 px-3 py-1 rounded">
-          Sub-Category
+        <button
+          onClick={() => setOpenAddSubCategory(true)}
+          className="text-sm border border-primary-200 hover:bg-primary-200 px-3 py-1 rounded"
+        >
+          Add Sub-Category
         </button>
       </div>
-    </div>
+
+      {openAddSubCategory && (
+        <UploadSubCategoryModel close={() => setOpenAddSubCategory(false)} />
+      )}
+    </section>
   );
 };
 
