c9fef10d9fdae832d7918fc04baef7c25bbba185
RohanPengonda <rpengonda1@gmail.com>
2025-02-16 19:27:00 +0530

    Updated

--- Client/src/pages/SubCategoryPage.jsx
+++ Client/src/pages/SubCategoryPage.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const SubCategoryPage = () => {
-  return <div>SubCategoryPage</div>;
+  return (
+    <div>
+      <div className="py-2  bg-white shadow-md flex items-center justify-between">
+        <h2 className="font-semibold">Sub-Category</h2>
+        <button className="text-sm border border-primary-200 hover:bg-primary-200 px-3 py-1 rounded">
+          Sub-Category
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default SubCategoryPage;
