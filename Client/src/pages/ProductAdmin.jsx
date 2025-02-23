cd353a5023ad3c106b7b55cce7aae37e30f3e38e
RohanPengonda <rpengonda1@gmail.com>
2025-02-23 22:54:57 +0530

    Updated

--- Client/src/pages/ProductAdmin.jsx
+++ Client/src/pages/ProductAdmin.jsx
@@ -4,12 +4,14 @@ import AxiosToastError from "../utils/AxiosToastError";
 import Axios from "../utils/Axios";
 import Loading from "../components/Loading";
 import ProductCardAdmin from "../components/ProductCardAdmin";
+import { IoSearchOutline } from "react-icons/io5";
 
 const ProductAdmin = () => {
   const [productData, setProductData] = useState([]);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
   const [totalPageCount, setTotalPageCount] = useState(1);
+  const [search, setSearch] = useState("");
 
   const fetchProductData = async () => {
     try {
@@ -47,18 +49,35 @@ const ProductAdmin = () => {
     }
   };
 
+  const handleOnChange = (e) => {
+    const { value } = e.target;
+    setSearch(value);
+  };
+
   return (
     <section>
-      <div className="p-2  bg-white shadow-md flex items-center justify-between">
+      <div className="p-2  bg-white shadow-md flex items-center justify-between gap-4">
         <h2 className="font-semibold">Product</h2>
+        <div className="h-full min-w-24 bg-blue-50 px-4 flex items-center gap-3 py-2 border rounded focus-within:border-primary-200">
+          <IoSearchOutline size={25} />
+          <input
+            className="h-full outline-none bg-transparent "
+            type="text"
+            onChange={handleOnChange}
+            value={search}
+            placeholder="Search Product Here ......"
+          />
+        </div>
       </div>
       {loading && <Loading />}
 
       <div className="p-4 bg-blue-50">
-        <div className="grid grid-cols-2 lg:grid-cols-6 md:grid-cols-4 gap-4">
-          {productData.map((p, index) => {
-            return <ProductCardAdmin data={p} />;
-          })}
+        <div className="min-h-[55vh]">
+          <div className="grid grid-cols-2 lg:grid-cols-6 md:grid-cols-4 gap-4 ">
+            {productData.map((p, index) => {
+              return <ProductCardAdmin data={p} />;
+            })}
+          </div>
         </div>
 
         <div className="flex justify-between my-4">
