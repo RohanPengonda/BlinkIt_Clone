45e61dcb8f9dbe58c33f2a92ed96e2a9db0ec765
RohanPengonda <rpengonda1@gmail.com>
2025-02-27 22:03:51 +0530

    Updated

--- Client/src/pages/ProductListPage.jsx
+++ Client/src/pages/ProductListPage.jsx
@@ -6,6 +6,8 @@ import AxiosToastError from "../utils/AxiosToastError";
 import Loading from "../components/Loading";
 import CardProduct from "../components/CardProduct";
 import { useSelector } from "react-redux";
+import { validURLConvert } from "../utils/validURLConvert";
+import { Link } from "react-router-dom";
 
 const ProductListPage = () => {
   const [data, setData] = useState([]);
@@ -71,21 +73,33 @@ const ProductListPage = () => {
 
   return (
     <section className="sticky top-24 lg:h-20">
-      <div className="container sticky top-24 mx-auto grid grid-cols-[90px,1fr] md:grid-cols-[200px,1fr] lg:grid-cols-[280px,1fr]">
-        {/* subcategory  */}
-        <div className=" min-h-[79vh]">
+      <div className="container sticky top-24  mx-auto grid grid-cols-[90px,1fr]  md:grid-cols-[200px,1fr] lg:grid-cols-[280px,1fr]">
+        {/**sub category **/}
+        <div className=" min-h-[88vh] max-h-[88vh] overflow-y-scroll  grid gap-1 shadow-md scrollbarCustom bg-white py-2">
           {displaySubCategory.map((s, index) => {
+            const link = `/${validURLConvert(s?.category[0]?.name)}-${
+              s?.category[0]?._id
+            }/${validURLConvert(s.name)}-${s._id}`;
             return (
-              <div className="w-full p-2">
-                <div className="w-full">
+              <Link
+                key={index}
+                to={link}
+                className={`w-full p-2 lg:flex items-center lg:w-full lg:h-16 box-border lg:gap-4 border-b 
+                  hover:bg-green-100 cursor-pointer
+                  ${subCategoryId === s._id ? "bg-green-100" : ""}
+                `}
+              >
+                <div className="w-fit max-w-28 mx-auto lg:mx-0 bg-white rounded  box-border">
                   <img
                     src={s.image}
                     alt="subCategory"
-                    className="w-15 h-full object-scale-down"
+                    className=" w-14 lg:h-14 lg:w-12 h-full object-scale-down"
                   />
                 </div>
-                <p>{s.name}</p>
-              </div>
+                <p className="-mt-6 lg:mt-0 text-xs text-center lg:text-left lg:text-base">
+                  {s.name}
+                </p>
+              </Link>
             );
           })}
         </div>
@@ -93,11 +107,11 @@ const ProductListPage = () => {
         {/* product  */}
 
         <div className="">
-          <div className="bg-white shadow-md p-2">
+          <div className="bg-white shadow-md p-2 z-10">
             <h3 className="font-semibold p-4">{subCategoryName}</h3>
           </div>
-          <div className="">
-            <div className="grid  grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-4 gap-4">
+          <div className="min-h-[80vh] max-h-[70vh] overflow-auto">
+            <div className="grid  grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-4 gap-4 ">
               {data.map((p, index) => {
                 return (
                   <CardProduct
