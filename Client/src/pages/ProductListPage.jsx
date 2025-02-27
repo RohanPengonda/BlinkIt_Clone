5fd1c1852e46332c1f8a589ca0d354a9d43e5b34
RohanPengonda <rpengonda1@gmail.com>
2025-02-27 21:09:22 +0530

    Updated

--- Client/src/pages/ProductListPage.jsx
+++ Client/src/pages/ProductListPage.jsx
@@ -3,6 +3,9 @@ import { useParams } from "react-router-dom";
 import SummaryApi from "../common/SummaryApi";
 import Axios from "../utils/Axios";
 import AxiosToastError from "../utils/AxiosToastError";
+import Loading from "../components/Loading";
+import CardProduct from "../components/CardProduct";
+import { useSelector } from "react-redux";
 
 const ProductListPage = () => {
   const [data, setData] = useState([]);
@@ -11,15 +14,18 @@ const ProductListPage = () => {
   const [totalPage, setTotalPage] = useState(1);
   const params = useParams();
 
+  const AllSubCategory = useSelector((state) => state.product.allSubCategory);
+
+  const [displaySubCategory, setDisplaySubCategory] = useState([]);
+
   const subCategory = params?.subCategory?.split("-");
   const subCategoryName = subCategory
     ?.slice(0, subCategory.length - 1)
     ?.join(" ");
-  console.log(subCategoryName);
+  const categoryId = params.category.split("-").slice(-1)[0];
+  const subCategoryId = params.subCategory.split("-").slice(-1)[0];
 
   const fetchProductData = async () => {
-    const categoryId = params.category.split("-".slice(-1)[0]);
-    const subCategoryId = params.subCategory.split("-").slice(-1)[0];
     try {
       setLoading(true);
       const response = await Axios({
@@ -52,17 +58,57 @@ const ProductListPage = () => {
   useEffect(() => {
     fetchProductData();
   }, [params]);
+
+  useEffect(() => {
+    const sub = AllSubCategory.filter((s) => {
+      const filterData = s.category.some((el) => {
+        return el._id == categoryId;
+      });
+      return filterData ? filterData : false;
+    });
+    setDisplaySubCategory(sub);
+  }, [params, AllSubCategory]);
+
   return (
     <section className="sticky top-24 lg:h-20">
       <div className="container sticky top-24 mx-auto grid grid-cols-[90px,1fr] md:grid-cols-[200px,1fr] lg:grid-cols-[280px,1fr]">
         {/* subcategory  */}
-        <div className=" min-h-[79vh]"></div>
+        <div className=" min-h-[79vh]">
+          {displaySubCategory.map((s, index) => {
+            return (
+              <div className="w-full p-2">
+                <div className="w-full">
+                  <img
+                    src={s.image}
+                    alt="subCategory"
+                    className="w-15 h-full object-scale-down"
+                  />
+                </div>
+                <p>{s.name}</p>
+              </div>
+            );
+          })}
+        </div>
 
         {/* product  */}
 
         <div className="">
           <div className="bg-white shadow-md p-2">
-            <h3 className="font-semibold">{subCategoryName}</h3>
+            <h3 className="font-semibold p-4">{subCategoryName}</h3>
+          </div>
+          <div className="">
+            <div className="grid  grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-4 gap-4">
+              {data.map((p, index) => {
+                return (
+                  <CardProduct
+                    data={p}
+                    key={p._id + "productSubCategory" + index}
+                  />
+                );
+              })}
+            </div>
+
+            {loading && <Loading />}
           </div>
         </div>
       </div>
